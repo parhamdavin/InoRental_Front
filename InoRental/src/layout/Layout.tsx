c3bbdbbe961651f3d7592176d41cb4f50fc300ed
Parham Davin <parhamdavinn3674@gmail.com>
2025-06-05 12:45:37 +0330

    fix bugs.

--- InoRental/src/layout/Layout.tsx
+++ InoRental/src/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./Footer/Footer";
-import Navbar from "../Navbar/Navbar";
+import Navbar from "./Navbar/Navbar";
 
 function Layout() {
   const location = useLocation();
